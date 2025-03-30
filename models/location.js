5358fa52e639d4feaa3ac2bc30934630cbfb359e
Sunny Sagar <sunnysagar620@gmail.com>
2025-03-30 13:20:00 +0530

    commit the changes of constraint

--- models/location.js
+++ models/location.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const locationSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
+    latitude: { type: Number,  default: null },
+    longitude: { type: Number,  default: null },
 });
 
 module.exports = mongoose.model("Location", locationSchema);
